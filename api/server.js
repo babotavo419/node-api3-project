b26f0dcc7b808d7b705a8dc03fa6da94b707e243
babotavo419 <one.faith419@gmail.com>
2023-08-02 05:18:03 -0400

    ok

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const usersRouter = require('./users/users-router');
-const postsRouter = require('./posts/posts-router');
 const { logger } = require('./middleware/middleware');
 
 const server = express();
@@ -13,7 +12,6 @@ server.use(logger);
 
 // route handlers
 server.use('/api/users', usersRouter);
-server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
